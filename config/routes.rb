a7d633b4671092f77137dc897e65975e3e7506cf
RaduGa <adraduga@gmail.com>
2015-01-04 17:49:46 +0300

    Создан вход/выход

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Cmw::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   match '/static_pages', to: 'static_pages#home', via: 'get'
   match '/signup',       to: 'users#new',         via: 'get'
-
+  match '/sessions',     to: 'sessions#new',   via: 'get'
+  match '/signin',       to: 'sessions#new',      via: 'get'
+  match '/signout',      to: 'sessions#destroy',  via: 'delete'
 
 
 
