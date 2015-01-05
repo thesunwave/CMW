71bbe9cddc14f7928db1d36c6cda82f3ab2567ae
RaduGa <adraduga@gmail.com>
2015-01-05 17:55:58 +0300

    Некоторые доработки

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
-      redirect_back_or user
+    @user = User.new(user_params)
+    if @user.save
+      redirect_back_or @user
     else
-      flash.now[:error] = 'Неправильный логин или пароль'
       render 'new'
     end
   end
