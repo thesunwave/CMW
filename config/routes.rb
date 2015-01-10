cd55601d0281848b64eddbdc85eef54fe058f3a6
RaduGa <adraduga@gmail.com>
2015-01-10 17:44:07 +0300

    up

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Cmw::Application.routes.draw do
   match '/signin',       to: 'sessions#new',      via: 'get'
   match '/signout',      to: 'sessions#destroy',  via: 'delete'
 
-  get '/:nikname' => 'users#show', as: :profile
+  get '/:nikname' =>      'users#show', as: :profile
+  get '/:nikname/edit' => 'users#edit', as: :edit
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
