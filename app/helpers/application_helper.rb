7b4292058e71183162bef15fb31df592185f7b75
TheSunwave <thesunwave@yandex.com>
2015-04-19 20:13:48 +0300

    [add] Lang helper for quote

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def quote_text(quote)
+    if current_user.lang == 'ru'
+      quote.text_rus
+    else
+      quote.text_eng
+    end
+  end
+
+  def quote_author(quote)
+    if current_user.lang == 'ru'
+      quote.author_rus
+    else
+      quote.author_eng
+    end
+  end
 end
