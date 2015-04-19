f3c9060f5f6f0fbe7b49a1014b388d23f3cc7bce
TheSunwave <thesunwave@yandex.com>
2015-04-19 22:02:27 +0300

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,25 @@
 module ApplicationHelper
   def quote_text(quote)
-    if current_user.lang == 'ru'
-      quote.text_rus
+    if current_user.present?
+      if current_user.lang == 'ru'
+        quote.text_rus
+      else
+        quote.text_eng
+      end
     else
-      quote.text_eng
+      quote.text_rus
     end
   end
 
   def quote_author(quote)
-    if current_user.lang == 'ru'
-      quote.author_rus
+    if current_user.present?
+      if current_user.lang == 'ru'
+        quote.author_rus
+      else
+        quote.author_eng
+      end
     else
-      quote.author_eng
+      quote.author_rus
     end
   end
 end
