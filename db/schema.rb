147ccf8bc3d3ab00ab8ff9ebd86e738044d1a5c9
Artur Borisovich <thesunwave@ya.ru>
2015-05-03 20:35:12 +0000

    [fix] Set average as float

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502203901) do
+ActiveRecord::Schema.define(version: 20150503193344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,6 +145,7 @@ ActiveRecord::Schema.define(version: 20150502203901) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.float    "average"
   end
 
 end
