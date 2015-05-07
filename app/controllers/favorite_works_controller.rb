8ba0b3f83182c6cae67285384aecb6cbbaafb935
TheSunwave <thesunwave@yandex.com>
2015-05-07 21:41:11 +0300

    [add] Favorite works

--- app/controllers/favorite_works_controller.rb
+++ app/controllers/favorite_works_controller.rb
@@ -1,9 +1,13 @@
 class FavoriteWorksController < ApplicationController
   before_action :set_work
 
+  def index
+    @favorites = Favorite.where(user_id: current_user.id)
+  end
+
   def create
     if Favorite.create(favorited: @work, user: current_user)
-      redirect_to show_work_path(current_user.username, @work), notice: 'Work has been favorited'
+      redirect_to :back, notice: 'Work has been favorited'
     else
       redirect_to show_work_path(current_user.username, @work), alert: 'Something went wrong...*sad panda*'
     end
@@ -11,7 +15,7 @@ class FavoriteWorksController < ApplicationController
 
   def destroy
     Favorite.where(favorited_id: @work.id, user_id: current_user.id).first.destroy
-    redirect_to show_work_path(current_user.username, @work), notice: 'Project is no longer in favorites'
+    redirect_to :back, notice: 'Work  is no longer in favorites'
   end
 
   private
--- app/helpers/favorite_works_helper.rb
+++ app/helpers/favorite_works_helper.rb
@@ -1,12 +1,15 @@
 module FavoriteWorksHelper
-
   def add_to_favs(work)
-    unless current_user.favorite_works.exists?(id: work.id)
-      link_to "В избранное!", favorite_works_path(username: work.user.username ,work_id: work.id),
-      method: :post
+    unless current_user.nil?
+      unless current_user.favorite_works.exists?(id: work.id)
+        link_to "В избранное!", favorite_works_path(username: work.user.username ,work_id: work.id),
+        method: :post
+      else
+        link_to 'Удалить из избранного', favorite_work_path(id: work.id, username: work.user.username ,work_id: work.id),
+        method: :delete
+      end
     else
-      link_to 'Удалить из избранного', favorite_works_path(username: work.user.username ,work_id: work.id),
-      method: :delete
+      link_to 'В избранное', new_user_session_path
     end
   end
 end
