f65bfec188e9d26c967a4b7ebec0fd7bc9ec59f1
zombiQWERTY <zombiqwerty@yandex.ru>
2015-05-11 01:10:12 +0300

    Added svgs into works

--- app/helpers/favorite_works_helper.rb
+++ app/helpers/favorite_works_helper.rb
@@ -5,11 +5,21 @@ module FavoriteWorksHelper
         link_to "В избранное!", favorite_works_path(username: work.user.username ,work_id: work.id),
         method: :post
       else
-        link_to 'Удалить из избранного', favorite_work_path(id: work.id, username: work.user.username ,work_id: work.id),
-        method: :delete
+        "В избранном<div class=\"b-new__service__item_favourites__remove\">" + link_to('(убрать)', favorite_work_path(id: work.id, username: work.user.username ,work_id: work.id), method: :delete) + "</div>"
       end
     else
       link_to 'В избранное', new_user_session_path
+    end
+  end
+  def check_fav(work)
+    unless current_user.nil?
+      unless current_user.favorite_works.exists?(id: work.id)
+        "b-new__service__item_favourites"
+      else
+        "b-new__service__item_favourites-added"
+      end
+    else
+      "b-new__service__item_favourites"
     end
   end
 end
