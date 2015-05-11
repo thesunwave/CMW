2017f42f58b2c0f9ed7fdd8d8ada02b671144d9f
zombiQWERTY <zombiqwerty@yandex.ru>
2015-05-11 15:50:48 +0300

    Fav link styled two steps

--- app/helpers/favorite_works_helper.rb
+++ app/helpers/favorite_works_helper.rb
@@ -5,7 +5,7 @@ module FavoriteWorksHelper
         link_to "В избранное!", favorite_works_path(username: work.user.username ,work_id: work.id),
         method: :post
       else
-        "В избранном<div class=\"b-new__service__item_favourites__remove\">" + link_to('(убрать)', favorite_work_path(id: work.id, username: work.user.username ,work_id: work.id), method: :delete) + "</div>"
+        "В избранном <div class=\"b-new__service__item_favourites__remove\">" + link_to('(убрать)', favorite_work_path(id: work.id, username: work.user.username ,work_id: work.id), method: :delete) + "</div>"
       end
     else
       link_to 'В избранное', new_user_session_path
